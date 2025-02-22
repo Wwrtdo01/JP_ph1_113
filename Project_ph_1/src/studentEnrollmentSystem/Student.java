16691b4cefe4329a9ec9035081856428ce7510c0
Wwrtdo01 <wks010101@gmail.com>
2025-02-23 01:28:59 +0300

    I cleaned everything since we will start coding as an adults tomorrow

--- Project_ph_1/src/studentEnrollmentSystem/Student.java
+++ Project_ph_1/src/studentEnrollmentSystem/Student.java
@@ -1,20 +1,6 @@
 package studentEnrollmentSystem;
 
 public class Student {
-	private String Stu;
-	
-	public void testMethod() {
-		System.out.println("123");
-		System.out.println("456"); //test
-		System.out.println(7); //rtreaggaz
-		System.out.println("jfhjfjfijf");
-		System.out.println("jfhjfjfijf");
-		System.out.println("jfhjffijf");
-		System.out.println("jfhjfjfijf");
-		System.out.println("jfhjfjfijf");
-		System.out.println("jfhjffijf");
-		System.out.println("jfhjfjfijf");
-		System.out.println("jfhjfjfijf");
-		System.out.println("jfhjfjfijf");
+
 	}
-}
+
--- Project_ph_1/src/studentEnrollmentSystem/StudentSystemDemo.java
+++ Project_ph_1/src/studentEnrollmentSystem/StudentSystemDemo.java
@@ -3,33 +3,7 @@ package studentEnrollmentSystem;
 public class StudentSystemDemo {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello world");
-		System.out.println("Hello");
-		System.out.println("ABDULLAH , AHMED");
-		System.out.println("ABDULLAH");
-		System.out.println("ABDULLAH , AHMED");
-		System.out.println(" , AHMED");
-		System.out.println(" , AHMED");
-		System.out.println("fjfjfjt");
-		System.out.println("aaaa");
-		System.out.println("The best team ever ever ever!!!!!!!!!!!!!!!!!!!!!!!!");
-		System.out.println("The best team ever ever ever!!!!!!!!!!!!!!!!!!!!!!!!");
 
-		System.out.println("The best team ever ever ever!!!!!!!!!!!!!!!!!!!!!!!!");
-
-		System.out.println("The best team ever ever ever!!!!!!!!!!!!!!!!!!!!!!!!");
-		
-		
-		int a = 5;
-		int b = 2;
-		System.out.println(5/2);
-		System.out.println("hhhhhhhhhhhhhhhhhhhhhhhh");
-		System.out.println("YEEEEEEEES");
-		System.out.println("hhhhhhhhhhhhhhhhhhhhhhhh: =?"+a/b);
-
-		
-		
 	}
 
 }

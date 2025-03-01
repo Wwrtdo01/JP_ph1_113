ee826a24018186ce960de05081aa0f5243bec032
Wwrtdo01 <wks010101@gmail.com>
2025-03-01 22:04:54 +0300

    FacultyMember class & Student class are done

--- Project_ph_1/src/ISstudentEnrollmentSystem/FacultyMember.java
+++ Project_ph_1/src/ISstudentEnrollmentSystem/FacultyMember.java
@@ -1,5 +1,14 @@
 package ISstudentEnrollmentSystem;
 
-public class FacultyMember {
+public abstract class FacultyMember extends Person{
+public String MembershipType;  // teaching assistant or facultyMember or professor
+
+public abstract String getSectionDoctor(); // to get your course teacher (who's gonna teach you), randomly as (KSU)
+
+// constructors 
+FacultyMember(String mType, String fname,String lName, int phoneNum, String email, int ssn, String location){
+	
+	MembershipType = mType;
+}
 
 }
--- Project_ph_1/src/ISstudentEnrollmentSystem/Student.java
+++ Project_ph_1/src/ISstudentEnrollmentSystem/Student.java
@@ -1,6 +1,22 @@
 package ISstudentEnrollmentSystem;
 
-public class Student {
+public class Student extends Person {
+protected Course courses[]; //Array of object & it's available courses at IS(information system) section [composition]
+protected int numOfCourse; // recorded courses
+protected ClassRoom cRoom[];  
+protected int id_S;         // Student id
+protected double GPA;     // the GPA of CFY : common first year
+protected String uniEmail;   // university email of student
+protected double SATdeg,SAATdeg;  //  SAT: قدرات. SAAT : تحصيلي.  
+
+
+//constructors 
+
+Student(String firstN,String lN, int phoneNum,String email, int ssn, String location ,int id, double gpa, String uniEmail, double sat, double saat){
+	id_S = id; GPA = gpa; this.uniEmail = uniEmail; SATdeg = sat ; SAATdeg = saat;
+}
+
+
 
 	}
 

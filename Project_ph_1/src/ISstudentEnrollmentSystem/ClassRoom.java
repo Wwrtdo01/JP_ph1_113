5786eb0d6c98787d8f82a96f357b2d302bf94ea7
AbdullahMkh05 <abdullahmkh05@gmail.com>
2025-03-03 21:46:00 +0300

    uptade

--- Project_ph_1/src/ISstudentEnrollmentSystem/ClassRoom.java
+++ Project_ph_1/src/ISstudentEnrollmentSystem/ClassRoom.java
@@ -1,5 +1,47 @@
 package ISstudentEnrollmentSystem;
 
 public class ClassRoom {
+	  private int buildingNum;  // Building number where the classroom is located
+	    private int floorNum;     // Floor number of the classroom
+	    private int classRoomNum; // Unique classroom number
+
+	    // Constructor
+	    public ClassRoom(int buildingNum, int floorNum, int classRoomNum) {
+	        this.buildingNum = buildingNum;
+	        this.floorNum = floorNum;
+	        this.classRoomNum = classRoomNum;
+	    }
+
+	    // Getters and Setters
+	    public int getBuildingNum() {
+	        return buildingNum;
+	    }
+
+	    public void setBuildingNum(int buildingNum) {
+	        this.buildingNum = buildingNum;
+	    }
+
+	    public int getFloorNum() {
+	        return floorNum;
+	    }
+
+	    public void setFloorNum(int floorNum) {
+	        this.floorNum = floorNum;
+	    }
+
+	    public int getClassRoomNum() {
+	        return classRoomNum;
+	    }
+
+	    public void setClassRoomNum(int classRoomNum) {
+	        this.classRoomNum = classRoomNum;
+	    }
+
+	    // Display classroom details
+	    public void displayClassRoomInfo() {
+	        System.out.println("Building Number: " + buildingNum);
+	        System.out.println("Floor Number: " + floorNum);
+	        System.out.println("Classroom Number: " + classRoomNum);
+	    }
 
 }
--- Project_ph_1/src/ISstudentEnrollmentSystem/Course.java
+++ Project_ph_1/src/ISstudentEnrollmentSystem/Course.java
@@ -1,5 +1,47 @@
 package ISstudentEnrollmentSystem;
 
 public class Course {
+	   private String courseName; // Name of the course
+	    private String courseId;   // Unique ID of the course
+	    private int courseHours;   // Credit hours for the course
 
-}
+	    // Constructor
+	    public Course(String courseName, String courseId, int courseHours) {
+	        this.courseName = courseName;
+	        this.courseId = courseId;
+	        this.courseHours = courseHours;
+	    }
+
+	    // Getters and Setters
+	    public String getCourseName() {
+	        return courseName;
+	    }
+
+	    public void setCourseName(String courseName) {
+	        this.courseName = courseName;
+	    }
+
+	    public String getCourseId() {
+	        return courseId;
+	    }
+
+	    public void setCourseId(String courseId) {
+	        this.courseId = courseId;
+	    }
+
+	    public int getCourseHours() {
+	        return courseHours;
+	    }
+
+	    public void setCourseHours(int courseHours) {
+	        this.courseHours = courseHours;
+	    }
+
+	    // Display course details
+	    public void displayCourseInfo() {
+	        System.out.println("Course Name: " + courseName);
+	        System.out.println("Course ID: " + courseId);
+	        System.out.println("Course Hours: " + courseHours);
+	    }
+	}
+
